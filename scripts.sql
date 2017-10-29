758ef2f372e152cebb1915a745dbd43c1bdee48e
Mohamed Abubucker <bucker@tmmmt.sa>
2017-10-29 15:12:48 +0530

    script generated from SQL SERVER directly

--- scripts.sql
+++ scripts.sql
@@ -1,34 +1,167 @@
-use employee
-go
-
+USE [master]
+GO
+/****** Object:  Database [EMPLOYEE]    Script Date: 10/29/2017 3:10:59 PM ******/
+CREATE DATABASE [EMPLOYEE] ON  PRIMARY 
+( NAME = N'EMPLOYEE', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10.SQLEXPRESS\MSSQL\DATA\EMPLOYEE.mdf' , SIZE = 2048KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'EMPLOYEE_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10.SQLEXPRESS\MSSQL\DATA\EMPLOYEE_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+ALTER DATABASE [EMPLOYEE] SET COMPATIBILITY_LEVEL = 100
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [EMPLOYEE].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [EMPLOYEE] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [EMPLOYEE] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [EMPLOYEE] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [EMPLOYEE] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [EMPLOYEE] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [EMPLOYEE] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [EMPLOYEE] SET  MULTI_USER 
+GO
+ALTER DATABASE [EMPLOYEE] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [EMPLOYEE] SET DB_CHAINING OFF 
+GO
+USE [EMPLOYEE]
+GO
+/****** Object:  Table [dbo].[departments_master]    Script Date: 10/29/2017 3:11:00 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[departments_master](
-[DEPT_CODE] int PRIMARY KEY,
-[DEPT_NAME] nvarchar(100),
-[DEPT_ADDR] nvarchar(200)
-)
+	[DEPT_CODE] [int] NOT NULL,
+	[DEPT_NAME] [nvarchar](100) NULL,
+	[DEPT_ADDR] [nvarchar](200) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[DEPT_CODE] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
-go
-
+GO
+/****** Object:  Table [dbo].[designations_master]    Script Date: 10/29/2017 3:11:00 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[designations_master](
-[DESGIN_ID] int PRIMARY KEY,
-[DESIGNATION] nvarchar(30) not null,
-[DESCRIPTION] nvarchar(100) null
-)
+	[DESGIN_ID] [int] NOT NULL,
+	[DESIGNATION] [nvarchar](30) NOT NULL,
+	[DESCRIPTION] [nvarchar](100) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[DESGIN_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
-go
-
+GO
+/****** Object:  Table [dbo].[employee]    Script Date: 10/29/2017 3:11:00 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[employee](
-[ID] int PRIMARY KEY,
-[NAME] nvarchar(50),
-[DOJ] datetime,
-[MOBILE] varchar(10),
-[EMAIL] varchar(30),
-[SALARY] money,
-DESIGN int FOREIGN KEY REFERENCES [designations_master]([DESGIN_ID]), 
-DEPT int FOREIGN KEY REFERENCES [departments_master]([DEPT_CODE]) 
-)
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[NAME] [nvarchar](50) NULL,
+	[DOJ] [datetime] NULL,
+	[MOBILE] [varchar](10) NULL,
+	[EMAIL] [varchar](30) NULL,
+	[SALARY] [money] NULL,
+	[DESIGN] [int] NULL,
+	[DEPT] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
-go
+GO
+INSERT [dbo].[departments_master] ([DEPT_CODE], [DEPT_NAME], [DEPT_ADDR]) VALUES (101, N'HR', N'Human Resource Management Department')
+GO
+INSERT [dbo].[departments_master] ([DEPT_CODE], [DEPT_NAME], [DEPT_ADDR]) VALUES (102, N'Finance', N'Accounts and FInance Department')
+GO
+INSERT [dbo].[departments_master] ([DEPT_CODE], [DEPT_NAME], [DEPT_ADDR]) VALUES (103, N'Sales', N'Sale and Revenue Department')
+GO
+INSERT [dbo].[departments_master] ([DEPT_CODE], [DEPT_NAME], [DEPT_ADDR]) VALUES (104, N'Production', N'Production Department')
+GO
+INSERT [dbo].[departments_master] ([DEPT_CODE], [DEPT_NAME], [DEPT_ADDR]) VALUES (105, N'IT', N'Technical and IT Team')
+GO
+INSERT [dbo].[designations_master] ([DESGIN_ID], [DESIGNATION], [DESCRIPTION]) VALUES (201, N'Manager', N'Manger for the respected Department.')
+GO
+INSERT [dbo].[designations_master] ([DESGIN_ID], [DESIGNATION], [DESCRIPTION]) VALUES (202, N'Supervisor', N'Block Supervisor')
+GO
+INSERT [dbo].[designations_master] ([DESGIN_ID], [DESIGNATION], [DESCRIPTION]) VALUES (203, N'Incharge', N'Project Incharge')
+GO
+INSERT [dbo].[designations_master] ([DESGIN_ID], [DESIGNATION], [DESCRIPTION]) VALUES (204, N'Senior', N'Senior Employee')
+GO
+INSERT [dbo].[designations_master] ([DESGIN_ID], [DESIGNATION], [DESCRIPTION]) VALUES (205, N'Junior', N'Entry Level')
+GO
+SET IDENTITY_INSERT [dbo].[employee] ON 
+
+GO
+INSERT [dbo].[employee] ([ID], [NAME], [DOJ], [MOBILE], [EMAIL], [SALARY], [DESIGN], [DEPT]) VALUES (2, N'Mohamed Abubucker', CAST(N'2016-01-01T00:00:00.000' AS DateTime), N'8883990205', N'abubucker.sed@gmail.com', 35000.0000, 205, 105)
+GO
+INSERT [dbo].[employee] ([ID], [NAME], [DOJ], [MOBILE], [EMAIL], [SALARY], [DESIGN], [DEPT]) VALUES (3, N'Mohamed Abubucker', CAST(N'2017-07-01T00:00:00.000' AS DateTime), N'9080471513', N'abubucker.sed@gmail.com', 35000.0000, 204, 105)
+GO
+INSERT [dbo].[employee] ([ID], [NAME], [DOJ], [MOBILE], [EMAIL], [SALARY], [DESIGN], [DEPT]) VALUES (4, N'Mohamed Musheer', CAST(N'2017-01-10T00:00:00.000' AS DateTime), N'9790179540', N'muchcud@gmail.com', 20000.0000, 205, 105)
+GO
+SET IDENTITY_INSERT [dbo].[employee] OFF
+GO
+ALTER TABLE [dbo].[employee]  WITH CHECK ADD FOREIGN KEY([DEPT])
+REFERENCES [dbo].[departments_master] ([DEPT_CODE])
+GO
+ALTER TABLE [dbo].[employee]  WITH CHECK ADD FOREIGN KEY([DESIGN])
+REFERENCES [dbo].[designations_master] ([DESGIN_ID])
+GO
+/****** Object:  StoredProcedure [dbo].[createEmployee]    Script Date: 10/29/2017 3:11:00 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
 CREATE PROCEDURE [dbo].[createEmployee](
 @name nvarchar(50),
@@ -44,6 +177,8 @@ BEGIN
 	BEGIN TRANSACTION
 		insert into [dbo].[employee]([NAME],[DOJ],[MOBILE],[EMAIL],[SALARY],DESIGN,DEPT) values(@name,@doj,@mobile,@email,@salary,@design,@dept)
 	COMMIT TRANSACTION
+END
+
 
 
 
@@ -64,17 +199,8 @@ insert into [dbo].[designations_master]([DESGIN_ID],[DESIGNATION],[DESCRIPTION])
 select * from employee
 select * from designations_master
 select * from departments_master
-
-
-DROP TABLE [dbo].[employee]
-
-CREATE TABLE [dbo].[employee](
-[ID] int IDENTITY(1,1) PRIMARY KEY,
-[NAME] nvarchar(50),
-[DOJ] datetime,
-[MOBILE] varchar(10),
-[EMAIL] varchar(30),
-[SALARY] money,
-DESIGN int FOREIGN KEY REFERENCES [designations_master]([DESGIN_ID]), 
-DEPT int FOREIGN KEY REFERENCES [departments_master]([DEPT_CODE]) 
-)
+GO
+USE [master]
+GO
+ALTER DATABASE [EMPLOYEE] SET  READ_WRITE 
+GO
